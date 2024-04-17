7d986b2e70ee072548b2537fcac97d88180fab17
Emiliano Mora-Carrera <36041423+EmilianoMora@users.noreply.github.com>
2024-04-17 14:45:59 +0200

    Create f_index_BAM.sh

---
+++ picard/f_index_BAM.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#SBATCH --job-name=EM_index
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=500:00:00
+#SBATCH --mem-per-cpu=3800
+#SBATCH --nice=1000
+#SBATCH --partition=main
+#SBATCH -o ./logs/index.%j.out
+#SBATCH -e ./logs/index.%j.err
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=emiliano.mora@systbot.uzh.ch
+#SBATCH --exclude=compute001,computeGPU001,computeGPU002
+
+##  SET THE VARIABLES BELOW  ######################
+# Please specify the full path for each variable. #
+
+INPUT=$(ls *_mdup.bam)
+
+###################################################
+
+for i in ${INPUT[@]}; do
+~/emiliano_2/executables/samtools-1.10/samtools index $i; done
+
+mv GW2* ./splitted_BAMs
+mv *_mdup.bam ./marked_dup_BAMs
+mv marked_dup_metrics* ./metrics
+mv *bam ./merged_BAMs
+mv *bam.bai ./merged_BAMs
+#!/bin/bash
+#SBATCH --job-name=EM_index
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=500:00:00
+#SBATCH --mem-per-cpu=3800
+#SBATCH --nice=1000
+#SBATCH --partition=main
+#SBATCH -o ./logs/index.%j.out
+#SBATCH -e ./logs/index.%j.err
+
+##  SET THE VARIABLES BELOW  ######################
+# Please specify the full path for each variable. #
+
+INPUT=$(ls *_mdup.bam)
+
+###################################################
+
+for i in ${INPUT[@]}; do
+~/emiliano_2/executables/samtools-1.10/samtools index $i; done
+
+mv GW2* ./splitted_BAMs
+mv *_mdup.bam ./marked_dup_BAMs
+mv marked_dup_metrics* ./metrics
+mv *bam ./merged_BAMs
+mv *bam.bai ./merged_BAMs
